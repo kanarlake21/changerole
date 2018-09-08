e4ba8b34329c3c962fa9c13ccd4a7198dbefd287
kanarlake21 <43089539+kanarlake21@users.noreply.github.com>
2018-09-08 12:45:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ bot.on('message', message => {
 
 bot.on("guildMemberAdd", member => {
     member.guild.channels.find("name", "general").send('Bienvenue ${member}')
-})
+    })
 
 bot.on("guildMemberRemove", member => {
     member.guild.channels.find("name", "general").send('${member} vient de quitter')
-})
+    })
 
 bot.on('guildMemberAdd', member => {
     var role = member.guild.roles.find('name', 'Membres');
     member.addRole(role)
-})
+    })
+}
