63b75f126682185ae36fdde2e56ad036fbe3bff6
kanarlake21 <43089539+kanarlake21@users.noreply.github.com>
2018-09-08 12:49:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 var bot = new Discord.Client();
 bot.login("token");
-var prefix = ("*")
+var prefix = ("*");
 
 bot.on("guildMemberAdd", member => {
     member.guild.channels.find("name", "general").send('Bienvenue ${member}')
