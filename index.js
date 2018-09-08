8c6699af0aeeb15a5fb503d0d8023e5141030383
kanarlake21 <43089539+kanarlake21@users.noreply.github.com>
2018-09-08 12:42:48 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+var bot = new Discord.Client();
+bot.login("token");
+var prefix = ("*")
+
+bot.on('message', message => {
+
+bot.on("guildMemberAdd", member => {
+    member.guild.channels.find("name", "general").send('Bienvenue ${member}')
+})
+
+bot.on("guildMemberRemove", member => {
+    member.guild.channels.find("name", "general").send('${member} vient de quitter')
+})
+
+bot.on('guildMemberAdd', member => {
+    var role = member.guild.roles.find('name', 'Membres');
+    member.addRole(role)
+})
